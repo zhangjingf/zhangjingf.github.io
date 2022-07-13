79792b970edfd8bdf83af5e5b9e278b76cdfb892
zhangjingfeng <734546749@qq.com>
2022-07-13 15:57:13 +0800

    feat: test

--- src/views/express/express.vue
+++ src/views/express/express.vue
@@ -13,14 +13,21 @@
                     <el-col :span="20">
                         <el-form-item prop="code">
                             <el-select v-model="form.code" placeholder="请选择快递">
-                                <el-option v-for="(item, index) in expressList" :key="index" :value="item.code" :label="item.name">
+                                <el-option
+                                    v-for="(item, index) in expressList"
+                                    :key="index"
+                                    :value="item.code"
+                                    :label="item.name"
+                                >
                                 </el-option>
                             </el-select>
                         </el-form-item>
                     </el-col>
                 </el-row>
                 <el-row style="text-align:center">
-                    <el-button type="primary" size="medium" @click="fetchExpressLog">查询</el-button>
+                    <el-button type="primary" size="medium" @click="fetchExpressLog"
+                        >查询</el-button
+                    >
                 </el-row>
             </el-form>
         </div>
@@ -28,31 +35,39 @@
     </div>
 </template>
 <script>
-import {expressList} from '../../utils/constants';
 import axios from 'axios';
+import { expressList } from '../../utils/constants';
+
 export default {
-    data() {
-        return {
-            form: {},
-            expressList: expressList,
-            rules: {
-                number: [{ required: true, message: '请输入', trigger: 'blur' }],
-                code: [{ required: true, message: '请选择', trigger: 'blur' }]
-            }
-        };
-    },
-    methods: {
-        fetchExpressLog() {
-            this.$refs['form'].validate(val => {
-                if (val) {
-                    axios.get(`http://www.kuaidi100.com/query?type=${this.form.code}&postid=${this.form.number}`).then(res => {
-                        console.log(res);
-                    })
-                }
+  data() {
+    return {
+      form: {},
+      expressList,
+      rules: {
+        number: [{ required: true, message: '请输入', trigger: 'blur' }],
+        code: [{ required: true, message: '请选择', trigger: 'blur' }]
+      }
+    };
+  },
+  mounted() {
+    console.log(this.$http);
+  },
+  methods: {
+    fetchExpressLog() {
+      this.$refs.form.validate((val) => {
+        if (val) {
+          axios
+            .get(
+              `http://www.kuaidi100.com/query?type=${this.form.code}&postid=${this.form.number}`
+            )
+            .then((res) => {
+              console.log(res);
             });
         }
+      });
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
 .express {
